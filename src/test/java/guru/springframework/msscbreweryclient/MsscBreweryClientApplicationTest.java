a6e5333a10c380545533909876dbd3f8d6683a09
kamildyla <kamil.dyla09@gmail.com>
2020-12-19 18:13:11 +0100

    Initial project add. Closes #1

---
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTest.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+
+class MsscBreweryClientApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
